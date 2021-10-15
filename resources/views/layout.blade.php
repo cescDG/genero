1231e310fd39e73b4959330eb2c244baacfa1747
cescDG <cesar199403@hotmail.com>
2021-10-15 16:35:59 -0500

    libs sel

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -31,6 +31,8 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('genero/vendors/materialize-stepper/materialize-stepper.min.css')}} ">
     <link rel="stylesheet" type="text/css" href="{{ asset('genero/vendors/materialize-stepperUno/materialize-stepper.min.css')}} ">
     <link rel="stylesheet" type="text/css" href="{{ asset('genero/css-rtl/pages/form-wizard.css')}} ">
+    <link rel="stylesheet" href="{{ asset('genero/vendors/select2/select2.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('genero/vendors/select2/select2-materialize.css') }}" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
@@ -117,6 +119,8 @@
     <script src="{{ asset('genero/js/custom/custom-script.js')}}"></script>
     <script src="{{ asset('genero/vendors/materialize-stepper/materialize-stepper.min.js')}}"></script>
     <script src="{{ asset('genero/js/scripts/form-wizard.js')}}"></script>
+    <script src="{{ asset('genero/vendors/select2/select2.full.min.js') }}"></script>
+    <script src="{{ asset('genero/vendors/select2/select2.full.min.js') }}"></script>
     <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script type="text/javascript">
