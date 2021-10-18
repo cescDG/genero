92615da6635c0fcc365c8497a286ba2f98a7f545
cescDG <cesar199403@hotmail.com>
2021-10-18 14:58:31 -0500

    estilo tab

--- resources/views/reportes/show.blade.php
+++ resources/views/reportes/show.blade.php
@@ -13,71 +13,69 @@
             <div class="section section-data-tables">
                 <div class="row">
                     <div class="col s12">
-                        <h4 align="center">Preguntas</h4>
+                        <h4 align="center">{{$ubicacion->Nombre}}</h4>
                         @foreach ($preguntas as $pregunta)
+                            <table class="striped" style="border:1px solid;">
+
+                                <thead>
+                                    <tr>
+                                        <th style="width: 50%;">{{ $pregunta->texto }}</th>
+                                        <th>Sí</th>
+                                        <th>No</th>
+                                        <th>Algunas veces</th>
+                                        <th>Desconozo</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>Total</td>
+
+                                        <td>
+                                            @foreach ($sumaA as $key => $value)
+                                                @if ($key == $pregunta->id)
+                                                    {{ $value }}
+                                                @endif
+                                            @endforeach
+                                        </td>
 
 
-                        <table class="striped" style="border:1px solid;">
-    
-                            <thead>
-                                <tr>
-                                    <th>{{ $pregunta->texto }}</th>
-                                    <th>Sí</th>
-                                    <th>No</th>
-                                    <th>Algunas veces</th>
-                                    <th>Desconozo</th>
-                                </tr>
-                            </thead>
-    
-                            <tbody>
-                                <tr>
-                                    <td>Total</td>
-                                   
+
                                         <td>
-                                            @foreach($sumaA as $key => $value)
-                                                @if($key == $pregunta->id)
-                                                    {{$value}}
+                                            @foreach ($sumaB as $key => $value)
+                                                @if ($key == $pregunta->id)
+                                                    {{ $value }}
                                                 @endif
                                             @endforeach
                                         </td>
-                                       
-    
-                                  
+
+
+
                                         <td>
-                                            @foreach($sumaB as $key => $value)
-                                                @if($key == $pregunta->id)
-                                                {{$value}}
+                                            @foreach ($sumaC as $key => $value)
+                                                @if ($key == $pregunta->id)
+                                                    {{ $value }}
                                                 @endif
                                             @endforeach
                                         </td>
-                                     
-    
-                                   
+
+
                                         <td>
-                                            @foreach($sumaC as $key => $value)
-                                                @if($key == $pregunta->id)
-                                                {{$value}}
+
+                                            @foreach ($sumaD as $key => $value)
+                                                @if ($key == $pregunta->id)
+                                                    {{ $value }}
                                                 @endif
                                             @endforeach
                                         </td>
-                                      
-    
-                                        <td>
-    
-                                            @foreach($sumaD as $key => $value)
-                                                @if($key == $pregunta->id)
-                                                        {{$value}}
-                                                @endif
-                                            @endforeach
-                                        </td>
-                                      
-                                </tr>   
-                                
-                            </tbody>
-                        </table>
+
+                                    </tr>
+
+                                </tbody>
+                            </table>
                             <br>
-    
-                     @endforeach
+
+                        @endforeach
                         <div class="card-body">
                             <div class="container">
                                 <br>
@@ -92,5 +90,3 @@
         </div>
     </div>
 </div>
-
-
